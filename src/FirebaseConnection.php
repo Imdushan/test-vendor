e53f00cef315a7ff86d448a859b633e5b27a0a10
ImDushan <ineshmadhushan@gmail.com>
2023-12-26 12:45:44 +0400

    Initial commit

---
+++ src/FirebaseConnection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MyModule\Firebase;
+
+use Kreait\Laravel\Firebase\Facades\Firebase as FirebaseFacade;
+
+class FirebaseConnection
+{
+    private $database;
+
+    private function __construct()
+    {
+        $this->database = FirebaseFacade::database();
+    }
+
+    public static function getInstance()
+    {
+        return new self();
+    }
+
+    public function getDatabase()
+    {
+        return $this->database;
+    }
+
+    public function getFirebase(): \Kreait\Firebase\Database
+    {
+        return $this->database;
+    }
+}
