4f777ecbeeb6dc96a576ba32ca3d9014e848e9da
ImDushan <ineshmadhushan@gmail.com>
2023-12-28 09:42:52 +0400

    dynamicRequest

---
+++ dynamicrequesthandler/src/DynamicRequestHandlerTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace YourVendor\MyModule;
+
+use InvalidArgumentException;
+
+class DynamicRequestHandlerTrait
+{
+    /**
+     * Get a value from the request, or throw an exception if not present.
+     *
+     * @param array  $request  The request array.
+     * @param string $key      The key to retrieve from the request.
+     * @param mixed  $default  The default value if the key is not present.
+     *
+     * @return mixed
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function getRequestValue(array $request, string $key, $default = null)
+    {
+        if (array_key_exists($key, $request)) {
+            return $request[$key];
+        }
+
+        if ($default !== null) {
+            return $default;
+        }
+
+        throw new InvalidArgumentException("Missing required attribute: {$key}");
+    }
+}
